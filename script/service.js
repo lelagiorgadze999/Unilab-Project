46fef4c8a22ca583ef486efd37be3fb1f30002b9
lelagiorgadze999 <lela.giorgadzedoit@geolab.edu.ge>
2024-04-09 22:49:00 +0400

    edit
    edit

--- script/service.js
+++ script/service.js
@@ -9,3 +9,13 @@ btns.forEach((btn) => {
     this.classList.add("btnactive");
   });
 });
+/////////////////////////
+//menu
+const menuIcon = document.querySelector(".man-icon");
+const menuBox = document.querySelector(".menu-box");
+
+menuIcon.addEventListener("click", function (e) {
+  // Toggle the visibility of the menu box
+  console.log("button");
+  menuBox.classList.toggle("active");
+});
